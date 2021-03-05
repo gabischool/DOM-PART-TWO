8da8f7a62da9c6ac534b5a8759eb89fc9da02fc7
qorrax <simplecarrier101@gmail.com>
2021-03-04 22:41:47 -0500

    mohamed-qorrax

--- js/index.js
+++ js/index.js
@@ -1,1 +1,57 @@
-// Your code goes here
+//  Your code goes here
+
+let topLogo = document.querySelector(".logo-heading")
+topLogo.addEventListener("mouseover", function(event)  {
+topLogo.style.color = "red"
+
+})
+
+
+
+let adventureHeader  = document.querySelector(".text-content h2")
+adventureHeader.addEventListener("dblclick", function(event)  {
+    adventureHeader.styl.colorColor = "blue"
+
+})
+
+
+
+
+let adventureHeader  = document.querySelector(".img-content")
+adventureHeader.addEventListener("dblclick", function(event)  {
+    adventureHeader.styl.colorColor = "blue"
+
+
+
+  const destinationPicture = document.querySelector(".content-destination h2")
+      destinationPicture.ddEventListener("focus", function(event) 
+      destinationPicture.styl.backgroundColor = "black"
+
+
+ 
+  
+ 
+ 
+ let funBUs = document.querySelector(".logo-heading")
+    funBUs.addEventListener('wheel', zoom);
+  
+    let navTitles = document.querrySelectorAll(".nav-link")
+        navTitles.forEach(nav => {
+            nav.addEventListener("dblclik" ,(myEvent) =>)
+            myEvent.style.backgroundColor = "purple"
+        });
+ 
+ const myBtn = document.querrySelectorAll(".btn")
+ myBtn.forEach(funtciont (items) {
+    items.addEventListener("click", funciton(myEvent) {
+    myEvent.target.style.backgroundColor = "purple"
+
+    })
+ 
+ })
+  
+   const adventureImage = document.querrySelector(".img.content img")
+   adventureImage.addEventListener("wheel" , (event) =>{
+    event.src = "adventureImage" 
+   }) 
+ 
