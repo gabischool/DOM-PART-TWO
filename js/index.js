28d66ba5510f2bf445139654e5ec0212ff2d79fb
IQRA-ABDI <yourname@domain.com>
2022-12-30 22:54:26 +0300

    i tried the assignment

--- js/index.js
+++ js/index.js
@@ -1,26 +1,60 @@
 // Your code goes here
-const earth=document.querySelector('.intro')
-earth.addEventListener("mouseover",()=>{
-earth. style.transform="scale(1)"
-earth. style.transform="all o.3s"
+const mouseOver = document.querySelector(".intro img");
+//mouseover
+mouseOver.addEventListener("mouseover", function(){
+mouseOver.style.transform = "scale(1.1)";
+   mouseOver.style.transition = "all 0.9s"
 })
+//mouseleave
+mouseOver.addEventListener("mouseleave", () => {
+    mouseOver.style.transform = "scale(0.9)"
+  })
 
-const changeText=document.querySelector(".intro")
-changeText.addEventListener("dblclick",()=>{
-changeText.textContent="Welcome To the Fun Bus!"
-})
+// click  
 
-const textdestination=document.querySelector(".footer")
-textdestination.addeventListener("resize",()=>{
-textdestination.textContent="Copyright Fun Bus 2022"
+let links = document.querySelector(".nav");
 
-})
-const contentdestination=document.querySelector(".content-destination")
-contentdestination.addEventListener('focus', (event) => {
-event.target.style.background = 'blue';
+links.addEventListener("click", function(event) {
+     event.preventDefault(); 
 });
 
-const mainnav=document.querySelctor(".main-navigation")
-mainnav.addeventListener("keydown",function(){
-mainnav.style.backgroundcolor="blue"
-})
+let txt= document.querySelector("h2");
+console.log(txt);
+txt.addEventListener("click", function(event) {
+    txt.style.color="red"
+});
+
+//dblclick
+txt.addEventListener("dblclick", function(event) {
+    txt.style.color="blue"
+});
+// pageload
+let body= document.querySelector("body");
+window.addEventListener("pageload", function(event) {
+    body.style.backgroundColor="blue"
+});
+
+// scroll
+let navs= document.querySelector(".main-navigation");
+window.addEventListener("scroll", ()=>{
+    navs.style.transition="all 0.9s"
+navs.style.display="sticky"
+navs.style.backgroundColor="lightblue"
+});
+
+//resize
+
+let image= document.querySelector(".img-content img");
+window.addEventListener("resize", ()=>{
+image.style.transform = "scale(0.9)";
+});
+
+
+
+
+
+
+
+
+
+
